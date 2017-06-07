1abc1fc5a31b886b8cf1695be1eba99e98850251
nirbu000 <nirbu000@sharklasers.com>
2017-06-07 19:09:29 +0300

    Add files via upload

---
+++ AV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AV
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Random random = new Random();
+            int num = random.Next(0, 15);
+
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
